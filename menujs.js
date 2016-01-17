c7d0f0eda9242d78a3dc9d9f0ebd52dff2ff26cf
KarolGruszecki <gruszkins@gmail.com>
2016-01-17 17:55:37 +0100

    add Html & js

---
+++ menujs.js
@@ -0,0 +1,9 @@
+function addli {
+
+var li = document.createElement("li")
+var ul = document.getElementsByTagName('ul') [0];
+li.innerText = "Developer";
+ul.appedChild(Developer);
+}
+
+addli("ul");
